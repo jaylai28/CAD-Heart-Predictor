c5674ba1766ec5bdf33877c46e0c25d1b66e99c3
Wei Jian <weijianlai@dyn-118-139-48-58.its.monash.edu.au>
2020-09-23 22:54:26 +1000

    Project

--- CAD Heart Disease.R
+++ CAD Heart Disease.R
@@ -26,29 +26,29 @@ new_CAD4 = na.omit(CAD4)
 new_CAD4
 
 
-#get all the categorical attributes and remove the NA values
-CAD4_cat = subset(new_CAD4, select = c(Sex,Obesity,CRF,CVA,`Airway.disease`,`Thyroid.Disease`,CHF,DLP,
-                                       `Weak.Peripheral.Pulse`,`Lung.rales`,`Systolic.Murmur`,`Diastolic.Murmur`,Dyspnea,Atypical,Nonanginal,
-                                       `Exertional.CP`,`LowTH.Ang`,LVH,`Poor.R.Progression`,BBB,VHD,Cath))
+# #get all the categorical attributes and remove the NA values
+# CAD4_cat = subset(new_CAD4, select = c(Sex,Obesity,CRF,CVA,`Airway.disease`,`Thyroid.Disease`,CHF,DLP,
+#                                        `Weak.Peripheral.Pulse`,`Lung.rales`,`Systolic.Murmur`,`Diastolic.Murmur`,Dyspnea,Atypical,Nonanginal,
+#                                        `Exertional.CP`,`LowTH.Ang`,LVH,`Poor.R.Progression`,BBB,VHD,Cath))
+# 
+# 
+# #get all the real-valued attributes and remove the NA values
+# CAD4_num = subset(new_CAD4, select = -c(Sex,Obesity,CRF,CVA,`Airway.disease`,`Thyroid.Disease`,CHF,DLP,
+#                                         `Weak.Peripheral.Pulse`,`Lung.rales`,`Systolic.Murmur`,`Diastolic.Murmur`,Dyspnea,Atypical,Nonanginal,
+#                                         `Exertional.CP`,`LowTH.Ang`,LVH,`Poor.R.Progression`,BBB,VHD,Cath))
+# 
+# CAD4_num
 
 
-#get all the real-valued attributes and remove the NA values
-CAD4_num = subset(new_CAD4, select = -c(Sex,Obesity,CRF,CVA,`Airway.disease`,`Thyroid.Disease`,CHF,DLP,
-                                        `Weak.Peripheral.Pulse`,`Lung.rales`,`Systolic.Murmur`,`Diastolic.Murmur`,Dyspnea,Atypical,Nonanginal,
-                                        `Exertional.CP`,`LowTH.Ang`,LVH,`Poor.R.Progression`,BBB,VHD,Cath))
-
-CAD4_num
-
-
-#plot a heatmap to see the correlation of all the variables
-factor_vars <- names(which(sapply(CAD4_num, class) == "factor"))
-numeric_vars <- setdiff(colnames(CAD4_num), factor_vars)
-numeric_vars <- setdiff(numeric_vars, "Cath")
-numeric_vars
-numeric_vars_mat <- as.matrix(CAD4_num[, numeric_vars, drop=FALSE])
-numeric_vars_cor <- cor(numeric_vars_mat)
-numeric_vars_cor
-corrplot(numeric_vars_cor)
+# #plot a heatmap to see the correlation of all the variables
+# factor_vars <- names(which(sapply(new_CAD4, class) == "factor"))
+# numeric_vars <- setdiff(colnames(new_CAD4), factor_vars)
+# numeric_vars <- setdiff(numeric_vars, "Cath")
+# numeric_vars
+# numeric_vars_mat <- as.matrix(new_CAD4[, numeric_vars, drop=FALSE])
+# numeric_vars_cor <- cor(numeric_vars_mat)
+# numeric_vars_cor
+# corrplot(numeric_vars_cor)
 
 
 
@@ -82,15 +82,15 @@ confusion_value = table$overall['Accuracy']
 confusion_value
 
 
-# define the control using a random forest selection function
-control = rfeControl(functions=rfFuncs, method="cv", number=10)
-# run the RFE algorithm
-results <- rfe(new_CAD4[,1:55], new_CAD4[,56], sizes=c(1:8), rfeControl=control)
-
-# summarize the results
-results
-# list the chosen features
-predictors(results)
+# # define the control using a random forest selection function
+# control = rfeControl(functions=rfFuncs, method="cv", number=10)
+# # run the RFE algorithm
+# results <- rfe(new_CAD4[,1:55], new_CAD4[,56], sizes=c(1:8), rfeControl=control)
+# 
+# # summarize the results
+# results
+# # list the chosen features
+# predictors(results)
 
 
 library(ROCR)
@@ -124,9 +124,10 @@ barplot(CAD4_randomforest$importance[order(CAD4_randomforest$importance, decreas
 #Perform cross validation for random forest
 CAD4_rfcv = rfcv(trainx = CAD4.train[,-c(19)],	trainy = CAD4.train[,c(19)],	cv.fold=10,	scale="log",	step=0.5)
 CAD4_rfcv
-CAD4_cvrandomforest = randomForest(Cath~., data = CAD4.train, na.action = na.exclude, ntree=500, mtry=20)
+CAD4_cvrandomforest = randomForest(Cath~., data = CAD4.train, na.action = na.exclude, ntree=500, mtry=17)
 CAD4_cvrandomforest
 CAD4_pred_cvRF = predict(CAD4_cvrandomforest, CAD4.test)
+
 
 #calculate the accuracy of random forest for cross validation with confusion matrix
 RF_confusion_matrix = table(Predicted_Class = CAD4_pred_cvRF, Actual_Class = CAD4.test$Cath)
